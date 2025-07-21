7ee4edaefd7d82dca7b62982b1878ae701e5541f
JMSagum <112904712+JMSGM@users.noreply.github.com>
2025-07-21 14:42:34 -0700

    Add background, add card animation, add fade when scroll, fix skills cards

---
+++ WebPort/js/main.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const fades = document.querySelectorAll('.fade-in');
+
+    const options = { threshold:0.1};
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting){
+                entry.target.classList.add('show');
+            }
+        });
+    }, options);
+
+    fades.forEach(el => observer.observe(el));
+
+});
